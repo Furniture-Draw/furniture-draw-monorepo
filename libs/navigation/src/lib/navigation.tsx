78ccac23d1235e832238eb4b1a190bcfc00fe95a
Yusuf Sargın <35580473+yusufsargin@users.noreply.github.com>
2024-12-02 19:50:08 +0300

    navigation has creatd

---
+++ libs/navigation/src/lib/navigation.tsx
@@ -0,0 +1,11 @@
+import styles from './navigation.module.css';
+
+export function Navigation() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Navigation!</h1>
+    </div>
+  );
+}
+
+export default Navigation;
