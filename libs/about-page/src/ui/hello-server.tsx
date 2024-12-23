4e7ba31267e864db04b016a16275d0ececf718ea
mustafaduyarer <mustafaduyarer@gmail.com>
2024-12-23 18:46:09 +0100

    AboutPage updated

---
+++ libs/about-page/src/ui/hello-server.tsx
@@ -0,0 +1,12 @@
+import { Navigation } from '@furniture-draw/navigation';
+
+
+export const AboutPage = () => {
+  return (
+    <div>
+      <Navigation />
+      <div style={{ color:'red' }}>Mobilplan's story</div>
+      <p>We have worked in all production, manufacturing and sales departments in the furniture industry for years. Our story started with a design and production program that reflects the experiences we have gained.</p>
+    </div>
+  );
+};
