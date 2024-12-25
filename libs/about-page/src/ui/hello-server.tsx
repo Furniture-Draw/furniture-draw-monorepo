91695c2b048f727c9ead4b558e0028b8f103aab1
mustafaduyarer <mustafaduyarer@gmail.com>
2024-12-25 14:34:52 +0100

    abut page updated

--- libs/about-page/src/ui/hello-server.tsx
+++ libs/about-page/src/ui/hello-server.tsx
@@ -1,12 +1,29 @@
+'use client';
 import { Navigation } from '@furniture-draw/navigation';
-
 
 export const AboutPage = () => {
   return (
     <div>
       <Navigation />
-      <div style={{ color:'red' }}>Mobilplan's story</div>
-      <p>We have worked in all production, manufacturing and sales departments in the furniture industry for years. Our story started with a design and production program that reflects the experiences we have gained.</p>
+      <div style={{ color: 'red' }}>Mobilplan's story</div>
+      
+
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        {/* Üstte resim */}
+        <img
+          src="libs\about-page\src\assets\image1.jpg"
+          alt="Sample"
+          style={{ width: '100%', maxWidth: '300px', borderRadius: '10px' }}
+        />
+
+        {/* Altta metin */}
+        <p style={{ marginTop: '10px', fontSize: '18px', color: '#333' }}>
+        We have worked in all production, manufacturing and sales departments in
+        the furniture industry for years. Our story started with a design and
+        production program that reflects the experiences we have gained.
+        </p>
+        
+      </div>
     </div>
   );
 };
