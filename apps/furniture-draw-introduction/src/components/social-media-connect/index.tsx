0d12c14f883006877a1a5da2503cd5e0ef48cc91
mustafaduyarer <mustafaduyarer@gmail.com>
2025-01-12 15:27:54 +0100

    sosyal media icons added

--- apps/furniture-draw-introduction/src/components/social-media-connect/index.tsx
+++ apps/furniture-draw-introduction/src/components/social-media-connect/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import './index.css';
 import { Link, Stack } from '@mui/material';
 import { Facebook, Google } from '@mui/icons-material';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export const SocialMediaConnect = () => {
   return (
-    <Stack spacing={4} className="container-social-connect">
+    <Stack spacing={2} className="container-social-connect">
+      <Link href="https://www.whatsapp.com">
+        <WhatsAppIcon
+          sx={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      </Link>
       <Link href="https://www.facebook.com">
         <Facebook
           sx={{
@@ -14,12 +24,23 @@ export const SocialMediaConnect = () => {
           }}
         />
       </Link>
-      <Google
-        sx={{
-          width: 40,
-          height: 40,
-        }}
-      />
+
+      <Link href="https://www.instagram.com">
+        <InstagramIcon
+          sx={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      </Link>
+      <Link href="https://www.whatsapp.com">
+        <Google
+          sx={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      </Link>
     </Stack>
   );
 };
--- libs/about-page/src/ui/hello-server.tsx
+++ libs/about-page/src/ui/hello-server.tsx
@@ -1,23 +1,39 @@
 'use client';
 import { Navigation } from '@furniture-draw/navigation';
+import "./aboutPage.css"
 
 export const AboutPage = () => {
   return (
     <div>
       <Navigation />
-      <div style={{ textAlign: 'center',color: 'red' }}>Mobilplan's story</div>
+      <div
+        style={{
+          marginTop: '30px',
+          marginBottom: '30px',
+          textAlign: 'center',
+          color: 'red',
+          fontSize: '28px',
+        }}
+      >
+        Mobilplan's story
+      </div>
+
+      <div className="container">
+        <img src="../images/image1.jpg" alt="aboutpage image1" />
+      </div>
       
-
-      <div style={{ alignItems:'center', padding: '20px' }}>
-        {/* Üstte resim */}
-        <img
-          src="../images/image1.jpg"
-          alt="Sample"
-          style={{ width: '100%', maxWidth: '700px', borderRadius: '60px' }}
-        />
-
+      <div>
         {/* Altta metin */}
-        <p style={{ marginTop: '10px', fontSize: '18px', color: '#333' }}>
+        <p
+          style={{
+            marginTop: '20px',
+            marginBottom: '60px',
+            fontSize: '18px',
+            color: '#333',
+            marginLeft: '30px',
+            marginRight: '40px',
+          }}
+        >
           We have worked in all production, manufacturing and sales departments
           in the furniture industry for years. Our story started with a design
           and production program that reflects the experiences we have gained.
